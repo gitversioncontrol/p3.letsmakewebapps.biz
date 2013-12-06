65470594459726555ba02ba40c0bbf63d178678e
Harpreet Singh <singhsacademics@gmail.com>
2013-12-06 14:55:04 -0500

    Code cleanup and styles fix

--- index.php
+++ index.php
@@ -1,23 +1,22 @@
 <!DOCTYPE html>
 <html>
 	<head>
-	<title>Investment Goal Estimator</title> 
-	<link rel="stylesheet" href="project3.css" type="text/css"> <!-- project 3 css-->
 	
+	<link rel="stylesheet" href="project3.css" type="text/css"> <!-- project 3 css-->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script> <!-- Jquery API-->
 	<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.js"></script> <!-- Jquery Validate plugin-->
 	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script> <!-- Jquery Ui interface plugin -->
 	<script type="text/javascript" src="http://www.workshop.rs/jqbargraph/jqBarGraph.js"></script><!-- Jquery bar-graph plugin-->
-	<!-- <script src="jqBarGraph.1.1.min.js"></script> -->
 	<link rel="stylesheet" href="http://www.workshop.rs/jqbargraph/styles.css" type="text/css" /> <!-- Jquery bar-graph css-->
+	<link rel="stylesheet" href="project3.css" type="text/css"> <!-- project 3 css-->
+	<title>Investment Goal Estimator</title> 
 	</head>
 	
 	<body>
-	<h1>  Investment Goal Estimator </h1>
+	<h1>Investment Goal Estimator </h1>
 		
 		<form id="userForm">
-		You can figure out the duration required to reach your target goal using this tool.
-		
+		You can figure out the duration required to reach your target goal using this tool.The bar-graph will show progress with duration chosen.
 		<p> Financial Goal: $<input  type="text" id="target" class="goal" name="Financial Goal"  maxlength ="10"  min="100"  required> <span class="slider"> </span> <span class="target"> </span> </p>
 		<p> Investment Duration: <input type="number"  id="duration"  class="goal" name="Investment Duration"  maxlength ="3" min="1" max="100" required> year(s) <span class="duration"></p> 
 		<p> Amount Currently Invested:  $<input type="text"  id="principal"  class="goal" name="Amount Currently Invested" maxlength ="10" required><span class="principal"> </span> </p>
@@ -32,6 +31,7 @@
 		
 		
 	<div id="output"></div>
+	
 	
 	<div id="divForGraph"></div>
 	
--- project3.js
+++ project3.js
@@ -1,122 +1,80 @@
 
-	/*$(function() {
-			   $( '.slider' ).slider({
-			  value:3,
-			  min: 100,
-			  max: 10000000,
-			  step: 100,
-			  slide: function( event, ui ) {
-				  $( "#target" ).val( ui.value );
-				 // $("#label").html(labelArr[ui.value]);
-			  }
-		  });
-		}
-	)*/
-	$('input,button').change(function(){
-					//console.log("Hi");
-					
-					//$('#output').html("Monthly Investment of $"+contribution+" will generate $");
-					validation();
-					customValidation();
-				//validation();
-								}
-					);
+	$('input,button').change(function(){ //When input change happens start event
+					validation(); //Form validation for Calculate button using Jquery validation plugin
+					customValidation(); //Custom Validation function
+										}
+							);
 					
 	
-	function validation(){
-	$('#userForm').validate({
-		 // debug: true
-				   submitHandler: function(form) {
-			// do other things for a valid form
-			//form.submit();
-			//customValidation();
-		  }
-		});
-		// alert( "Valid: " + form.valid() );
-		//customValidation();
-	
+	function validation(){//Form validation for Calculate button using Jquery validation plugin
+		$('#userForm').validate({
+		   submitHandler: function(form) { //If Validation is successful,submit form
+										}
+								});
+			
 	}
 	
-	function customValidation(){
+	function customValidation(){ //Input validation
 		$('#divForGraph').html(""); //To reset graph for live update when change of input is done.
 		var financialgoal=$('input[id=target]').val();
 		var principal=$('input[id=principal]').val();
 		var rate=$('input[id=rate]').val();
 		var duration=$('input[id=duration]').val();
-			var reason=""; //Variable to store input validation errors.
-									$('.goal').each(function(){
-									var name=$(this).attr('name'); //Pull  name for current input
-									var ids=$(this).attr('id'); //Pull id for current input
-									var length=$(this).val().length;
-									
-								//console.log("name is" + name + " and id is "+ids);
-								
-											if (length == 0) {
-											reason=reason+(name+" can't be empty");
-											$('.'+ids).html( name+ " can't be empty");
-													}
-													
-													
-												else 		if( ( ! $.isNumeric($(this).val()) ) || ($(this).val() < 0) ){
-															reason=reason+(name+ " should be a  number with non-negative value ");
-															console.log("reason-1 is "+reason);
-															$('.'+ids).html(name+ " should be a  number with non-negative value ");
-															}
-															else
-															{
-															$('.'+ids).html("");
-															//console.log($(this).val());
-															//calculate();
-															}
-															
-													
-													
-										//	console.log("reason-2 is "+reason);
-																												
-								}		
-							)
+		var reason=""; //Variable to store input validation errors.
+		
+			$('.goal').each(function(){ //Iterate thru each input field
+				var name=$(this).attr('name'); //Pull  name for current input
+				var ids=$(this).attr('id'); //Pull id for current input
+				var length=$(this).val().length;
+
+					if (length == 0) {//Check if the input field is empty
+					reason=reason+(name+" can't be empty");
+					$('.'+ids).html( name+ " can't be empty");
+							}
+					else 	if( ( ! $.isNumeric($(this).val()) ) || ($(this).val() < 0) ){ // check if the input field is a valid number
+								reason=reason+(name+ " should be a  number with non-negative value ");
+								$('.'+ids).html(name+ " should be a  number with non-negative value ");
+								}
+								else
+								{
+								$('.'+ids).html("");								
+								}
+																
+				}		
+			) //End of each function
 			
-			console.log("principal is "+principal+"financialgoal is "+financialgoal);
-			console.log("reason-2 before last test is "+reason);			
-			if ( parseInt(principal) >= parseInt(financialgoal) ){
-			 console.log("inside p> f test");
-										reason=reason+("Either raise your financial goal or move some money from your investment account to mine.");
-										$('.principal').html("Either raise your financial goal or move some money from your investment account to mine :) ");										
-										}
-				else
-				{
-				//reason="";
-			//	$('.principal').html("");
-				}
 				
-				
-			if ( rate <= 0 ){
-				reason=reason+("Rate can't be zero or negative.");
-				$('.rate').html("Rate can't be zero or negative.");
+			if ( parseInt(principal) >= parseInt(financialgoal) ){//Check if financial foal is less than already invested amount.
+				reason=reason+("Either raise your financial goal or move some money from your investment account to mine.");
+				$('.principal').html("Either raise your financial goal or move some money from your investment account to mine :) ");										
 			}
-			if ( financialgoal < 100){
-			reason=reason+("Raise Financial goal to be more than $100");
+						
+			if ( rate <= 0 || rate > 100){//Check if investment return rate is valid and should not be greater than 100 for practical results.
+				reason=reason+("Please settle rate in between 0.1-100%");
+				$('.rate').html("Please settle return rate in between 0.1-100%");
+			}
+			
+			if ( financialgoal < 100){ //Check to make sure financial foal is more than $100 for best results.
+				reason=reason+("Raise Financial goal to be more than $100");
 				$('.target').html("Raise Financial goal to be more than $100.");
 			}
-			if (duration <=0){
-			reason=reason+("Duration of investment can't be zero or negative");
-				$('.duration').html("Duration of investment can't be zero or negative.")
-			}
-										
-			console.log("reason-2 is "+reason);					
-										
-			if (reason == ""){
-							calculate();
-							}
-			else
-							{
-							$('#output').html("");
-							}
 			
-		
+			if (duration <=0 || duration > 100){ //Check to make sure duration is in between 0 and 100 years for best results
+				reason=reason+("Settle Duration of investment in between 1 to 100 years");
+				$('.duration').html("Settle Duration of investment in between 1 to 100 years");
+			}
+
+			if (reason == ""){ //If there is no validation error Run caluclate function to find results
+				calculate(); 
+			}
+			else
+			{
+				$('#output').html(""); //Clear output from screen if there is any input validation error.
+			}
+			
 	}//EOF customValidation
 					
-	function calculate(){
+	function calculate(){ //Calculation Logic
 		var financialgoal=$('input[id=target]').val();
 		var contribution=$('input[id=contribution]').val();
 		var principal=$('input[id=principal]').val();
@@ -124,131 +82,94 @@
 		var target=$('input[id=target]').val();
 		var rate=$('input[id=rate]').val();
 		var inflation=$('input[id=inflation]').val();
-		
 
-		console.log("from calculate fn "+contribution+" " +principal+" "+duration+" "+target+" "+rate);
-		
-		var totalObj=new gettotal(financialgoal,contribution,principal,duration,target,rate);
-		var Total=totalObj.Total;
-		var idealContribution=totalObj.idealContribution;
-		
-		
-		console.log("idealContribution is " +idealContribution);
-			if ( (parseInt(Total)) != (parseInt(financialgoal)) ){
-			$('#output').html("Monthly Investment of $"+contribution+" and amount already invested will result in total investment value to be "+ Total.toFixed(2)+"."); //toFixed to bring precision to 2 decimal
-			$('#output').append('<p> Monthly Contribution of $'+idealContribution.toFixed(2)+' and amount already invested will almost meet your goal </p>' );	
-			
+		var totalObj=new gettotal(financialgoal,contribution,principal,duration,target,rate);//Call gettotal method to get result object
+		var Total=totalObj.Total; //get Total from result object
+		var idealContribution=totalObj.idealContribution; //get Ideal contribution to reach target from result object
+
+			if ( (parseInt(Total)) != (parseInt(financialgoal)) ){ //Post output on screen
+			$('#output').html("*Monthly Investment of $"+contribution+" and amount already invested will result in total investment value to be "+ Total.toFixed(2)+"."); //toFixed to bring precision to 2 decimal
+			$('#output').append('<p> *Monthly Contribution of $'+idealContribution.toFixed(2)+' and amount already invested will almost meet your goal </p>' );	
 			}
 			else
 			{
-			$('#output').html("Congrats! Monthly Investment of $"+contribution+" will meet your goal of "+ Total.toFixed(2))
-			
+			$('#output').html("*Congrats! Monthly Investment of $"+contribution+" will meet your goal of "+ Total.toFixed(2))
 			}
 			
-			
+		//Logic for finding inflation erosion on total investment value	
 		var afterInflationTotal=( Total*(Math.pow( (1-(inflation/100) ),duration)) );
 		var afterInflationTotal = isNaN(parseInt(afterInflationTotal)) ? 0 : (afterInflationTotal); //If afterInflationTotal is Not allowed number ,then make it zero.
-			console.log(afterInflationTotal);
-			$('#output').append('<p> Value of total investment after inflation erosion will be: '+afterInflationTotal.toFixed(2) );	
+		$('#output').append('<p> *Value of total investment after inflation erosion will be: '+afterInflationTotal.toFixed(2) );	
 			
-			bargraph(financialgoal,contribution,principal,duration,target,rate);
+		//Run bargraph() to generate graph from data calculated	
+		bargraph(financialgoal,contribution,principal,duration,target,rate);
 		
-	}
+	}//EOF calculate
 	
 	
-	function gettotal(financialgoal,contribution,principal,duration,target,rate){
+	function gettotal(financialgoal,contribution,principal,duration,target,rate){//function to calculate total and idealContribution and bind it in a result object
 	
-	var principalGain=  ( (Math.pow( (1+rate/100),duration))  * principal);
-	var principalGain=isNaN(parseInt(principalGain)) ? 0 : (principalGain); //if principalGain is Nan(Not allowed number),make it zero.
-	
-	var monthlyInterest= (rate/12)/100 ;
-	console.log ("monthlyInterest "+ monthlyInterest);
-	var contributionGain= ( contribution * ( (Math.pow( (1+monthlyInterest),(duration*12) ))-1 )/monthlyInterest ) ;
-	var contributionGain = isNaN(parseInt(contributionGain)) ? 0 : (contributionGain); //If contributionGain is Not allowed number ,then make it zero.
-	console.log("principalGain is "+ principalGain);
-	console.log("contributionGain is "+ contributionGain);
-	var Total=principalGain+contributionGain;
-	//var idealContribution= ((financialgoal - principalGain)*monthlyInterest)/( (Math.pow( (1+monthlyInterest),(duration*12) ))-1 ) ;
-	var idealContribution= ( ((financialgoal - principalGain)*monthlyInterest)/( (Math.pow( (1+monthlyInterest),(duration*12) ))-1 ) );
-	var idealContribution = isNaN(parseInt(idealContribution)) ? 0 : (idealContribution); //If idealContribution is Not allowed number ,then make it zero.
-	//return Total;
-	this.Total=Total;
-	this.idealContribution=idealContribution;
-	
-	}
-	
-	 function bargraph(financialgoal,contribution,principal,duration,target,rate){
-		//var arrayOfData = [];
-		var totalobj=new gettotal(financialgoal,contribution,principal,duration,target,rate);
-		var idealContribution=totalobj.idealContribution;
-		console.log("idealContribution from gettotal method is: "+idealContribution);
+		var principalGain=  ( (Math.pow( (1+rate/100),duration))  * principal);
+		var principalGain=isNaN(parseInt(principalGain)) ? 0 : (principalGain); //if principalGain is Nan(Not allowed number),make it zero.
 		
-	 	/*arrayOfData = new Array(
-				 [[14,54],'2007'],
-				 [[8,48],'2008'],
-				 [[4,36],'2009']
-			); */
-			
-			var arrayOfData = [];
-			
-			var counter;
-			if ( duration > 10){ //If duration is more than 10 years then break it down to one tenth of duration to maintain graph count to 10 equal time units
-				counter=duration/10;
-			}
-			else{
-			counter=1;
-			}
-			
-			//for (var y=1; y<= duration ; y++){
-			for (var y=counter;y<= duration ; y=counter+y){
-				console.log("y is "+y);
-				y=parseInt(y);
-				console.log("y is "+y);
-				var totalobj2=new gettotal(financialgoal,contribution,principal,y,target,rate);
-				var Total=parseInt(totalobj2.Total);
-				
-				var totalobj_idealContribution=new gettotal(financialgoal,idealContribution,principal,y,target,rate);//Used IdealContribution here to find total investment value at different inteval
-				var Total_with_idealContribution=parseInt(totalobj_idealContribution.Total);
-				console.log("Total  from gettotal method is: "+Total+ "and with ideal contribution is : "+Total_with_idealContribution);
-					
-					
-					
-			
-				//for (var i =y; i++) {
-						
-						arrayOfData.push([[Total,Total_with_idealContribution],"Year:"+y]);
-			 
-						//}
-					
-			}
-			
-			console.log(arrayOfData);
-			//After you set your data array you just need to say in which div you want graph to be created. All you have to do is:
+		var monthlyInterest= (rate/12)/100 ; //Split annual rate into monthly rate
 
-			// var myarray= ['0','0'];
-			//$('#divForGraph').jqBarGraph({title: false});
-			//$(el).html('');
-			//$('#divForGraph').html("Progress of investment with your current contribution and calculated contribution to reach your goal");
-			$('#divForGraph').jqBarGraph({
-				
-				 data: arrayOfData,
-				 
-				 colors: ['#437346','#242424'] ,
-			animate: false,
-			type: 'multi',
-			   prefix: '$',
-			   barSpace: 20,
-			   width: '100%', // default width of your graph
-				height: 400, //default height of your graph
+		var contributionGain= ( contribution * ( (Math.pow( (1+monthlyInterest),(duration*12) ))-1 )/monthlyInterest ) ;
+		var contributionGain = isNaN(parseInt(contributionGain)) ? 0 : (contributionGain); //If contributionGain is Not allowed number ,then make it zero.
+		
+		var Total=principalGain+contributionGain;
+
+		var idealContribution= ( ((financialgoal - principalGain)*monthlyInterest)/( (Math.pow( (1+monthlyInterest),(duration*12) ))-1 ) );
+		var idealContribution = isNaN(parseInt(idealContribution)) ? 0 : (idealContribution); //If idealContribution is Not allowed number ,then make it zero.
+
+		this.Total=Total;
+		this.idealContribution=idealContribution;
+	
+	}//EOF gettotal
+	
+	function bargraph(financialgoal,contribution,principal,duration,target,rate){ //bargraph data generation and pushing it on graph plugin
+	
+		var totalobj=new gettotal(financialgoal,contribution,principal,duration,target,rate);//use duration defined by user to get gettotal object
+		var idealContribution=totalobj.idealContribution;//Pull idealContribution for the duration defined by user to reach target
+		var arrayOfData = [];
+		var counter;
+		
+		if ( duration > 10){ //If duration is more than 10 years then break it down to one tenth of duration to maintain graph count to 10 equal time-units
+			counter=duration/10;
+		}
+		else{
+		counter=1;
+		}
+
+		for (var y=counter;y<= duration ; y=counter+y){
+			y=parseInt(y); //To  roundup time-units
+			
+			var totalobj2=new gettotal(financialgoal,contribution,principal,y,target,rate);//Push broken time-unit for graph to get total at different intervals when contribution is user-defined
+			var Total=parseInt(totalobj2.Total);
+			
+			var totalobj_idealContribution=new gettotal(financialgoal,idealContribution,principal,y,target,rate);//Used IdealContribution here to find total investment value at different interval to reach target
+			var Total_with_idealContribution=parseInt(totalobj_idealContribution.Total);
+			
+			arrayOfData.push([[Total,Total_with_idealContribution],"Year:"+y]); //Push data in Array
+
+		}
+
+			$('#divForGraph').jqBarGraph({//Push array and other custom variables for graph plugin
+				data: arrayOfData,
+				colors: ['#437346','#810541'] ,
+				animate: false,
+				type: 'multi',
+				prefix: '$', //to show value in $
+				barSpace: 20,
+				width: '100%', // width of  graph 100% to cover whole screen
+				height: 400, // height of graph element
 				showValues: true,
 				showValuesColor: '#fff',
-				legends: ['Total Value with Current monthly Contribution of $'+contribution,"Total Value with Expected monthly Contribution of $"+idealContribution.toFixed(2)+" to reach goal"],
+				legends: ['Total Value with Current monthly Contribution of $'+contribution,"Total Value with Expected monthly Contribution of $"+idealContribution.toFixed(2)+" to reach goal of "+financialgoal],
 				legend: true
 			});
-	
-	 
-	 
-	 }
+
+	}//EOF bargraph
 				
 			
 	
